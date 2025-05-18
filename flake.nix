83217731f3ef154face65af5f48264d395a43b15
te9no <tatuys9428@gmail.com>
2025-05-18 18:33:23 +0900

    init

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
         board = "nice_nano_v2";
         shield = "lily58_%PART%";
 
-        zephyrDepsHash = "sha256-reAWLQgGuZQiCiN5yGoVlbN6CT5yxl7lEk2/HbvL2x4=";
+        zephyrDepsHash = "";
 
         meta = {
           description = "ZMK firmware";
